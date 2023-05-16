120e9d3e610be39ea77479f551b447abae65c174
samimhossain <samimhossainsanto@gmail.com>
2023-05-16 11:29:09 +0600

    gread

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-var jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
-
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-//middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
+
 
 
 
@@ -27,33 +25,31 @@ const client = new MongoClient(uri, {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
+    },
 });
+
 
 //============================
 //JWT verification
 //============================
 
-const veryfyJWT = (req, res, next) => {
-    console.log('hitting veryfy jwt');
-    console.log(req.headers.authorization);
-
+const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
     if (!authorization) {
-        return res.status(401).send({ error: true, message: 'unauthorized access' })
+        return res.status(401).send({ error: true, message: "Unauthorized access" });
     }
-    const token = authorization.split(' ')[1];
-    console.log('token inside verify jwt', token);
+
+    const token = authorization.split(" ")[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
         if (error) {
-            return res.status(401).send({ error: true, message: 'unauthorized access' })
+            return res.status(401).send({ error: true, message: "Unauthorized access" });
         }
 
         req.decoded = decoded;
         next();
-    })
+    });
+};
 
-}
 
 
 
@@ -66,16 +62,14 @@ async function run() {
         const bookingCollection = client.db('Car-Doctor').collection('bookings');
 
         //=============================
-        //jwt authorization
+        //JWT authorization route
         //=============================
 
-        app.post('/jwt', async (req, res) => {
+        app.post("/jwt", async (req, res) => {
             const user = req.body;
-            console.log(user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: '1h'
+                expiresIn: "1h",
             });
-            console.log(token);
             res.send({ token });
         });
 
@@ -84,20 +78,17 @@ async function run() {
         //services Service section
         //============================
 
-        app.get('/services', async (req, res) => {
+        app.get("/services", async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         });
 
 
-        app.get('/services/:id', async (req, res) => {
+        app.get("/services/:id", async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-
-            const options = {
-                projection: { title: 1, name: 1, price: 1, service_id: 1, img: 1 }
-            }
+            const query = { _id: new ObjectId(id) };
+            const options = { projection: { title: 1, name: 1, price: 1, service_id: 1, img: 1 } };
             const result = await serviceCollection.findOne(query, options);
             res.send(result);
         });
@@ -108,24 +99,20 @@ async function run() {
         //bookings Service section
         //==============================================
 
-        app.get('/bookings', veryfyJWT, async (req, res) => {
-            // console.log(req.headers.authorization);
-            console.log('come back after verification');
-            let query = {}
+        app.get("/bookings", verifyJWT, async (req, res) => {
+            let query = {};
             if (req.query?.email) {
-                query = { email: req.query.email }
+              query = { email: req.query.email };
             }
-            const result = await bookingCollection.find().toArray();
+            const result = await bookingCollection.find(query).toArray();
             res.send(result);
-        });
+          });
 
-        app.post('/bookings', async (req, res) => {
+          app.post("/bookings", async (req, res) => {
             const booking = req.body;
-            console.log(booking);
             const result = await bookingCollection.insertOne(booking);
             res.send(result);
-
-        });
+          });
 
 
         //==============================
