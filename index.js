cd502106b492b8baf8b237b4b21556faf66c42c5
samimhossain <samimhossainsanto@gmail.com>
2023-05-23 22:11:05 +0600

    serch

--- index.js
+++ index.js
@@ -79,7 +79,21 @@ async function run() {
         //============================
 
         app.get("/services", async (req, res) => {
-            const cursor = serviceCollection.find();
+            const sort = req.query.sort;
+            const search = req.query.search;
+            console.log(search);
+            // const query = {price: {$gte:100}};
+
+            const query = { title: {$regex: search, $options:'i'} }
+
+            const options = {
+
+                sort: {
+                    "price": sort === "asc" ? 1 : -1
+                }
+
+            }
+            const cursor = serviceCollection.find(query, options);
             const result = await cursor.toArray();
             res.send(result);
         });
